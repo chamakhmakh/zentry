415a8c114fb1c95649155507fc026457a81bd44a
farouk <bensaidf232@gmail.com>
2024-12-18 13:51:03 +0100

    start the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl text-cyan font-black">wlecome to zentry</h1>
+    </main>
+  );
+};
+
+export default App;
