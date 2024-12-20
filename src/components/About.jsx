a14028b779a440f6020b9312b6bfe2a09dbef98e
farouk <bensaidf232@gmail.com>
2024-12-20 13:45:08 +0100

    navbar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,7 +26,7 @@ const About = () => {
   });
 
   return (
-    <div id="about" className="min-h-screen w-screen">
+    <div id="About" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
         <h2 className="font-general text-sm uppercase mf:text-[10px]">
           Welcome to zentry
