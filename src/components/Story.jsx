b509f78310f115ed99c91d615294ed9025262775
farouk <bensaidf232@gmail.com>
2024-12-21 12:34:43 +0100

    done

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,22 +1,12 @@
-import React, { useRef } from "react";
-import AnimatedTitle from "./AnimatedTitle";
 import gsap from "gsap";
-import RoundedCorners from "./RoundedCorners";
+import { useRef } from "react";
+
 import Button from "./Button";
+import AnimatedTitle from "./AnimatedTitle";
+import RoundedCorners from "./RoundedCorners";
 
-const Story = () => {
-  const frameRef = useRef("null");
-
-  const handleMouseLeave = () => {
-    const element = frameRef.current;
-
-    gsap.to(element, {
-      duration: 0.3,
-      rotateX: 0,
-      rotateY: 0,
-      ease: "power1.inOut",
-    });
-  };
+const FloatingImage = () => {
+  const frameRef = useRef(null);
 
   const handleMouseMove = (e) => {
     const { clientX, clientY } = e;
@@ -25,37 +15,51 @@ const Story = () => {
     if (!element) return;
 
     const rect = element.getBoundingClientRect();
-
-    const x = clientX - rect.left;
-    const y = clientY - rect.top;
+    const xPos = clientX - rect.left;
+    const yPos = clientY - rect.top;
 
     const centerX = rect.width / 2;
     const centerY = rect.height / 2;
 
-    const rotateX = ((y - centerY) / centerY) * -10;
-    const rotateY = ((x - centerX) / centerX) * 10;
+    const rotateX = ((yPos - centerY) / centerY) * -10;
+    const rotateY = ((xPos - centerX) / centerX) * 10;
 
     gsap.to(element, {
       duration: 0.3,
-      rotateX: rotateX,
-      rotateY: rotateY,
+      rotateX,
+      rotateY,
       transformPerspective: 500,
       ease: "power1.inOut",
     });
   };
 
+  const handleMouseLeave = () => {
+    const element = frameRef.current;
+
+    if (element) {
+      gsap.to(element, {
+        duration: 0.3,
+        rotateX: 0,
+        rotateY: 0,
+        ease: "power1.inOut",
+      });
+    }
+  };
+
   return (
-    <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+    <div
+      id={"story" && "Prologue"}
+      className="min-h-dvh w-screen bg-black text-blue-50"
+    >
       <div className="flex size-full flex-col items-center py-10 pb-24">
-        <p className="font-general text-sm uppercase">
+        <p className="font-general text-sm uppercase md:text-[10px]">
           the multiversal ip world
         </p>
 
         <div className="relative size-full">
           <AnimatedTitle
-            title="The st<b>o</b>ry of <br /> a hidden real<b>m</b>"
-            sectionId="#story"
-            containerClass="mt-5 pointer-evevnts-none mix-blend-difference relative z-10"
+            title="the st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
           />
 
           <div className="story-img-container">
@@ -63,12 +67,12 @@ const Story = () => {
               <div className="story-img-content">
                 <img
                   ref={frameRef}
+                  onMouseMove={handleMouseMove}
                   onMouseLeave={handleMouseLeave}
                   onMouseUp={handleMouseLeave}
                   onMouseEnter={handleMouseLeave}
-                  onMouseMove={handleMouseMove}
                   src="/img/entrance.webp"
-                  alt="entrance"
+                  alt="entrance.webp"
                   className="object-contain"
                 />
               </div>
@@ -87,15 +91,15 @@ const Story = () => {
             </p>
 
             <Button
-              id="realm-button"
+              id="realm-btn"
               title="discover prologue"
               containerClass="mt-5"
             />
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
-export default Story;
+export default FloatingImage;
